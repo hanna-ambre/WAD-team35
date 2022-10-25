320f3405d05c671e09684a43905b5aef7a7768f8
Hanna Saskia Ambre <hanna.s.ambre@gmail.com>
2022-10-25 04:00:22 +0300

    Tasks 4 and 5: javascript fetches JSON from folder and displays posts

---
+++ src/js/script.js
@@ -0,0 +1,68 @@
+window.onload = function() {
+
+
+//  JSON file hosted on jsonbin.io https://api.jsonbin.io/v3/b/63526b7a2b3499323be5f46d
+
+    //fetch('https://api.jsonbin.io/v3/b/63526b7a2b3499323be5f46d'){
+    fetch('res/json/myjson.json')
+        .then((response) => response.json())
+        .then(posts => {
+
+            const div_middle = document.getElementById('posts_div');
+
+            for (let post of posts) {
+                console.log(posts);
+                const div_post = document.createElement('div');
+                div_post.classList.add('post');
+                const div_post_Header = document.createElement('div');
+                div_post_Header.classList.add('post-Header');
+                const div_post_nav = document.createElement('div');
+                div_post_nav.classList.add('post-nav');
+                const div_post_body = document.createElement('div');
+                div_post_body.classList.add('post-body');
+
+                const avatar = document.createElement('a');
+                avatar.classList.add('avatar');
+                const avatar_image = document.createElement('img');
+                avatar_image.src = post.avatar;
+                avatar.appendChild(avatar_image);
+                div_post_nav.appendChild(avatar);
+
+                const name = document.createElement('a');
+                name.textContent = post.name;
+                div_post_nav.appendChild(name);
+                div_post_Header.appendChild(div_post_nav);
+
+                const date = document.createElement('a');
+                date.textContent = post.date;
+                div_post_Header.appendChild(date);
+
+                const text = document.createElement('p');
+                text.textContent = post.text;
+                div_post_body.appendChild(text);
+
+                if (post.image != "") {
+                    const image_link = document.createElement('a');
+                    const image = document.createElement('img');
+                    image.classList.add('post-pictures');
+                    image.src = post.image;
+                    image_link.appendChild(image);
+                    div_post_body.appendChild(image_link);
+                }
+
+                div_post.appendChild(div_post_Header);
+                div_post.appendChild(div_post_body);
+
+
+
+                div_middle.appendChild(div_post);
+            }
+        })
+        .catch(err => {
+            let errDiv = document.createElement("div");
+            errDiv.classList.add('post');
+            errDiv.innerText = err;
+            document.getElementById('posts_div').appendChild(errDiv);
+        })
+
+}
